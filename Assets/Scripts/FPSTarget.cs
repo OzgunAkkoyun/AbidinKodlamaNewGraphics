05cf79a41126b03815d50f54b2a7a523f3e063db
Özgün Akkoyun <35327622+OzgunAkkoyun@users.noreply.github.com>
2021-03-30 13:09:51 +0300

    Vsync closed

--- Assets/Scripts/FPSTarget.cs
+++ Assets/Scripts/FPSTarget.cs
@@ -5,6 +5,9 @@ public class FPSTarget : MonoBehaviour
     void Start()
     {
         Application.targetFrameRate = 60;
+        QualitySettings.vSyncCount = 0;
+
+        
         //Screen.SetResolution(640, 480, true);
     }
 }
