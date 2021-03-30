6c31d4e6ad901f92e98791fe107bc7e8c6ed506d
Özgün Akkoyun <35327622+OzgunAkkoyun@users.noreply.github.com>
2021-03-30 11:46:27 +0300

    More asmdef configuration

---
+++ Assets/Scripts/FPSTarget.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class FPSTarget : MonoBehaviour
+{
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        //Screen.SetResolution(640, 480, true);
+    }
+}
